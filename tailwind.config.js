fed4ef0ab323bc548dfabdda00e6b6589b60e44e
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2021-01-02 22:06:12 +0700

    🦚 Tailwind Config (#53)
    Adding additional TailwindCSS config and set default sans font into Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      'sans': ['Inter', ...defaultTheme.fontFamily.sans],
+      'serif': [...defaultTheme.fontFamily.serif],
+      'mono': [...defaultTheme.fontFamily.mono]
+    },
     extend: {},
   },
   variants: {
