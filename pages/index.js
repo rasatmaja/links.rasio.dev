d0407849b91275a78fa44ef85fe55455b358e60c
rasatmaja <rasioatmaja29@gmail.com>
2021-01-04 16:26:42 +0700

    add logic to switch dark mode

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 import Head from 'next/head'
 import styles from '../styles/home.module.css'
+import { useEffect, useState } from "react"
+import { useTheme } from "next-themes"
 
 export default function Home() {
+  const [isMounted, setIsMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const switchTheme = () => {
+    if (isMounted) {
+      setTheme(theme === "light" ? "dark" : "light");
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -10,13 +25,45 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <button
+          aria-label="Toggle Dark Mode"
+          type="button"
+          className="bg-gray-200 dark:bg-gray-800 rounded p-3 h-10 w-10 m-2"
+          onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+        >
+          {isMounted && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              stroke="currentColor"
+              className="h-4 w-4 text-gray-800 dark:text-gray-200"
+            >
+              {theme === 'dark' ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                />
+              )}
+            </svg>
+          )}
+        </button>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className="p-3 text-base rounded bg-gray-200 dark:bg-gray-800 dark:text-gray-200">pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
@@ -57,7 +104,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 fill-current dark:text-white" />
         </a>
       </footer>
     </div>
