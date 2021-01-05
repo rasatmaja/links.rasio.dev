83dbf0fe1d0d580ca859821c953070eeddbabba9
rasatmaja <rasioatmaja29@gmail.com>
2021-01-05 22:04:30 +0700

    add header component

---
+++ components/header.js
@@ -0,0 +1,14 @@
+export default function Header(){
+    return (
+        <header className="p-3">
+            <nav className="max-w-screen-md mx-auto flex justify-between">
+                <div>
+                    Home
+                </div>
+                <div>
+                    Dark mode toogle
+                </div>
+            </nav>
+        </header>
+    )
+}
