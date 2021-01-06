224a05f521797add2fa968ff79e5915481372d58
rasatmaja <rasioatmaja29@gmail.com>
2021-01-06 20:32:22 +0700

    change color bassed primary and secondary

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,7 @@ import Header from '../components/header'
 import Footer from '../components/footer'
 export default function Layout({children}){
     return (
-        <div>
+        <div className="bg-primary-light dark:bg-primary-dark text-gray-700 dark:text-gray-200 min-h-screen transition-colors duration-500">
             <Header></Header>
             {children}
             <Footer></Footer>
