a7dc6f849c2017b5968a1b75ab4c1d5774764b7c
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2021-01-06 15:45:23 +0700

    🦥 Footer Component (#64)
    Create footer component separate from layout

---
+++ components/footer.js
@@ -0,0 +1,19 @@
+import { getFontDefinitionFromNetwork } from "next/dist/next-server/server/font-utils";
+
+export default function Footer(){
+    return(
+        <footer className="p-10 border-gray-200 dark:border-gray-900 border-t-2">
+           <div className="max-w-screen-md mx-auto">
+                <p className="text-center text-sm">
+                    Develop with ❤️ by <span className="font-bold">Rasio Atmaja</span>
+                </p>
+                <p className="text-center text-sm">
+                    Build with <span className="font-bold">NextJS</span> and <span className="font-bold">TailwindCSS</span>
+                </p>
+                <p className="text-center text-sm">
+                    Hosted at <span className="font-bold">▲Vercel </span>
+                </p>
+           </div>
+        </footer>
+    )
+}
--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,11 @@
 import Header from '../components/header'
+import Footer from '../components/footer'
 export default function Layout({children}){
     return (
         <div>
             <Header></Header>
             {children}
-            <footer>
-                Footer
-            </footer>
+            <Footer></Footer>
         </div>    
     )
 }
