9cf0d036c0b4ded506e6ad5516a1df95dbbb712e
rasatmaja <rasioatmaja29@gmail.com>
2021-01-06 21:12:39 +0700

    remove padding on header tag

--- components/header.js
+++ components/header.js
@@ -1,9 +1,9 @@
 import DarkMode from './dark.mode.toggle'
 export default function Header(){
     return (
-        <header className="p-3">
-            <nav className="max-w-screen-md mx-auto flex justify-between">
-                <div className="flex items-center m-2">
+        <header>
+            <nav className="max-w-screen-md mx-auto flex justify-between p-5">
+                <div className="flex items-center">
                     Home
                 </div>
                 <div>
