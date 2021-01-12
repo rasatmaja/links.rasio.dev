aa85b76d4fbdbb1be74d0b58361386e1cb0c4dc7
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2021-01-12 20:42:39 +0700

    🐞 Fix Dark Mode Icons (#87)
    Fix dark mode icon state

--- components/dark.mode.toggle.js
+++ components/dark.mode.toggle.js
@@ -20,7 +20,7 @@ export default function DarkMode(){
         <button
           aria-label="Toggle Dark Mode"
           type="button"
-          className="bg-secondry-light dark:bg-secondry-dark rounded p-3 h-10 w-10"
+          className="bg-secondry-light dark:bg-secondry-dark rounded-lg h-10 w-10 flex justify-center items-center"
           onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
         >
           {isMounted && (
@@ -29,21 +29,21 @@ export default function DarkMode(){
               viewBox="0 0 24 24"
               fill="currentColor"
               stroke="currentColor"
-              className="h-4 w-4 text-gray-800 dark:text-gray-200"
+              className="h-5 w-5 text-gray-800 dark:text-gray-200"
             >
               {theme === 'dark' ? (
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth={2}
-                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
                 />
               ) : (
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth={2}
-                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
                 />
               )}
             </svg>
