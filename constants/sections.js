ca5b4dd1362dd4d2cb2bf3fba9edb56082a3a9cd
rasatmaja <rasioatmaja29@gmail.com>
2021-01-12 13:25:56 +0700

    add sections constant

---
+++ constants/sections.js
@@ -0,0 +1,20 @@
+const sections = [
+    {
+        name: 'CONTACT',
+        alias: 'Let’s Connect 🎯'
+    },
+    {
+        name: 'SOCIAL_MEDIA',
+        alias: 'Social Media 🎪'
+    },
+    {
+        name: 'HIGHLIGHT',
+        alias: 'Highlight 🔭'
+    },
+    {
+        name: 'COOL_STUFF',
+        alias: 'Cool Stuff 😎'
+    },
+]
+
+export default sections
