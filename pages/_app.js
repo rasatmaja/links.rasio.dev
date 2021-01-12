2cb35e3e1d975938b75e61f276a6f83f21a74a34
rasatmaja <rasioatmaja29@gmail.com>
2021-01-12 22:14:05 +0700

    fix lint and prettier

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,21 @@
-import '../styles/tailwind.css'
-import Layout from '../components/layout'
-import { ThemeProvider } from "next-themes";
+import '../styles/tailwind.css';
+import Layout from '../components/layout';
+import { ThemeProvider } from 'next-themes';
+import PropTypes from 'prop-types';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider attribute="class">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
-  )
+    return (
+        <ThemeProvider attribute="class">
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </ThemeProvider>
+    );
 }
 
-export default MyApp
+MyApp.propTypes = {
+    Component: PropTypes.func.isRequired,
+    pageProps: PropTypes.object.isRequired
+};
+
+export default MyApp;
